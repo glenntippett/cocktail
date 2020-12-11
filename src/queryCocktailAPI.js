654834e9bc28b2014633c22e6fd8e3719e4533c7
Glenn Tippett <glenn.tippett@gmail.com>
2020-12-12 09:00:43 +1100

    add error catches to api call

--- src/queryCocktailAPI.js
+++ src/queryCocktailAPI.js
@@ -3,17 +3,26 @@ const cocktailList = document.querySelector('.cocktail-list');
 
 const queryCocktailAPI = (cocktailName) => {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
-  .then(response => response.json())
+  .then(response => { 
+    // Check for bad/good http response
+    if (!response.ok) { console.log('error') }  
+    return response.json()})
   .then(data => {
-    data.drinks.forEach(drink => {
-      const cocktailNameList = `
-      <a class="img-cocktail" 
-      style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${drink.strDrinkThumb})"
-      href="#">
-      ${drink.strDrink}</a>
-      `;
-      cocktailList.insertAdjacentHTML('beforeend', cocktailNameList);
-    });
+    // Check if drink name entered is found
+    if (data.drinks === null) {
+      const noDrinksFound = '<p>No drinks found...</p>';
+      cocktailList.insertAdjacentHTML('beforeend', noDrinksFound);
+    } else {
+      data.drinks.forEach(drink => {
+        const cocktailNameList = `
+        <a class="img-cocktail" 
+        style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${drink.strDrinkThumb})"
+        href="#">
+        ${drink.strDrink}</a>
+        `;
+        cocktailList.insertAdjacentHTML('beforeend', cocktailNameList);
+      });
+    }
   });
 };
 
