930d043da806e4571f4bb300e9f13f03a752d0a9
Glenn Tippett <glenn.tippett@gmail.com>
2020-12-14 08:03:53 +1100

    refactor: separate build html function

--- src/queryCocktailAPI.js
+++ src/queryCocktailAPI.js
@@ -1,39 +1,41 @@
 const cocktailList = document.querySelector('.cocktail-list');
 
 
+const buildCocktailList = (data) => {
+  // Check if drink name entered is found
+  if (data.drinks === null) {
+    const noDrinksFound = '<p>No drinks found...</p>';
+    cocktailList.insertAdjacentHTML('beforeend', noDrinksFound);
+  } else {
+    data.drinks.forEach(drink => {
+      const cocktailNameList = `
+      <a class="img-cocktail" 
+      style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${drink.strDrinkThumb})"
+      href="#">
+      ${drink.strDrink}</a>
+      `;
+      cocktailList.insertAdjacentHTML('beforeend', cocktailNameList);
+    });
+  }
+}
+
 const queryCocktailAPI = (cocktailName) => {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
   .then(response => { 
     // Check for bad/good http response
     if (!response.ok) { console.log('error') }  
     return response.json()})
-  .then(data => {
-    // Check if drink name entered is found
-    if (data.drinks === null) {
-      const noDrinksFound = '<p>No drinks found...</p>';
-      cocktailList.insertAdjacentHTML('beforeend', noDrinksFound);
-    } else {
-      data.drinks.forEach(drink => {
-        const cocktailNameList = `
-        <a class="img-cocktail" 
-        style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${drink.strDrinkThumb})"
-        href="#">
-        ${drink.strDrink}</a>
-        `;
-        cocktailList.insertAdjacentHTML('beforeend', cocktailNameList);
-      });
-    }
+    .then(buildCocktailList);
+  };
+  
+  queryCocktailAPI('martini');
+  
+  const cocktailSearchForm = document.querySelector('.form-cocktail-search');
+  cocktailSearchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    cocktailList.innerHTML = "";
+    const cocktailName = document.querySelector('#input-cocktail-name').value;
+    queryCocktailAPI(cocktailName);
   });
-};
-
-queryCocktailAPI('martini');
-
-const cocktailSearchForm = document.querySelector('.form-cocktail-search');
-cocktailSearchForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  cocktailList.innerHTML = "";
-  const cocktailName = document.querySelector('#input-cocktail-name').value;
-  queryCocktailAPI(cocktailName);
-});
-
-export { queryCocktailAPI };
+  
+  export { queryCocktailAPI };
